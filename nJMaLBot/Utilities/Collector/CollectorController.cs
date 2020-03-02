858c0e8010aee5cdb9a979aa2c5d0b4afa5f793b
SKProCH <skproch@yandex.ru>
2020-03-02 17:32:18 +0300

    Collectors initial work

---
+++ nJMaLBot/Utilities/Collector/CollectorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Discord;
+using Timer = System.Threading.Timer;
+
+namespace Bot.Utilities.Collector {
+    public class CollectorController {
+        private Timer _timer;
+
+        public void SetTimeout(TimeSpan timeout) {
+            _timer = new Timer(state => { Dispose(); }, null, timeout, TimeSpan.FromSeconds(0));
+        }
+
+        public event EventHandler Stop;
+
+        public void Dispose() {
+            Stop?.Invoke(null, EventArgs.Empty);
+            _timer?.Dispose();
+        }
+    }
+}
