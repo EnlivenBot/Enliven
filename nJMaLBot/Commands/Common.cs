9205f0f58b1e0837de28dbf6e9498ac9a81d2b28
SKProCH <skproch@yandex.ru>
2020-03-06 08:15:02 +0300

    Add commands to retrieve statistics

--- nJMaLBot/Commands/Common.cs
+++ nJMaLBot/Commands/Common.cs
@@ -25,5 +25,43 @@ namespace Bot.Commands {
 
             await MessageHistoryManager.PrintLog(Convert.ToUInt64(messageId), channelId, (SocketTextChannel) Context.Channel, (IGuildUser) Context.User);
         }
+
+        [Command("stats", RunMode = RunMode.Async)]
+        [Summary("stats0s")]
+        public async Task Stats() {
+            await PrintStats(GlobalDB.CommandStatistics.FindById("Global"), null);
+        }
+
+        [Command("userstats", RunMode = RunMode.Async)]
+        [Summary("userstats0s")]
+        public async Task UserStats([Summary("userstats0_0s")] IUser user) {
+            await PrintStats(GlobalDB.CommandStatistics.FindById(user.Id.ToString()), user);
+        }
+
+        [Command("userstats", RunMode = RunMode.Async)]
+        [Summary("userstats1s")]
+        public async Task UserStats() {
+            await PrintStats(GlobalDB.CommandStatistics.FindById(Context.User.Id.ToString()), Context.User);
+        }
+
+        private async Task PrintStats(StatisticsPart stats, IUser user) {
+            var embedBuilder = new EmbedBuilder();
+            embedBuilder.WithColor(Color.Gold)
+                        .WithTitle(Loc.Get("Statistics.Title"))
+                        .WithDescription(user == null ? Loc.Get("Statistics.GlobalStats") : Loc.Get("Statistics.UserStats").Format(user.Username));
+            if (stats == null) {
+                embedBuilder.WithColor(Color.Red)
+                            .WithDescription(user == null ? Loc.Get("Statistics.NoGlobalStats") : Loc.Get("Statistics.NoUserStats").Format(user.Username));
+                await ReplyAsync(null, false, embedBuilder.Build());
+                return;
+            }
+
+
+            var valueTuples = stats.UsagesList.GroupBy(pair => HelpUtils.CommandAliases.Value[pair.Key].First().Name)
+                                   .Select(pairs => (pairs.Key.ToString(), pairs.Sum(pair => pair.Value)));
+            embedBuilder.AddField(Loc.Get("Statistics.ByCommands"),
+                string.Join("\n", valueTuples.Select((tuple, i) => $"`{tuple.Item1}` - {tuple.Item2}")));
+            await ReplyAsync(null, false, embedBuilder.Build());
+        }
     }
 }
