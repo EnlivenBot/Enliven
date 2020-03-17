0e4002f4114ce0eb423476de0e872ae3764d9c7f
SKProCH <skproch@yandex.ru>
2020-03-17 16:38:23 +0300

    Removed not working local lavalink

--- nJMaLBot/Music/MusicUtils.cs
+++ nJMaLBot/Music/MusicUtils.cs
@@ -28,94 +28,60 @@ namespace Bot.Music {
     public static class MusicUtils {
         public static LavalinkCluster Cluster;
         private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static EventLogger _lavalinkLogger;
 
         static MusicUtils() {
             Program.OnClientConnect += (sender, client) => { logger.Swallow(SetHandler()); };
+            _lavalinkLogger = new EventLogger();
+            _lavalinkLogger.LogMessage += (sender, args) => {
+                switch (args.Level) {
+                    case LogLevel.Information:
+                        logger.Info(args.Message);
+                        break;
+                    case LogLevel.Error:
+                        logger.Error(args.Exception, args.Message);
+                        break;
+                    case LogLevel.Warning:
+                        logger.Warn(args.Message);
+                        break;
+                    case LogLevel.Debug:
+                        logger.Info(args.Message);
+                        break;
+                    case LogLevel.Trace:
+                        logger.Trace(args.Message);
+                        break;
+                    default:
+                        logger.Warn(args.Message);
+                        break;
+                }
+            };
         }
 
         private static async Task SetHandler() {
             logger.Info("Starting music module");
-            var nodes = new List<LavalinkNodeOptions>();
-            if (GlobalConfig.Instance.IsSelfMusicEnabled) {
-                logger.Info("Preparing self music node");
-                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "Music"));
-                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Music", "application.yml"))) {
-                    logger.Info("Writing default application.yml");
-                    WriteResourceToFile("Bot.Music.application.yml", Path.Combine(Directory.GetCurrentDirectory(), "Music", "application.yml"));
-                }
-
-                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Music", "lavalink.jar"))) {
-                    await DownloadLavalink();
-                }
-                else if (File.GetCreationTime(Path.Combine(Directory.GetCurrentDirectory(), "Music", "lavalink.jar")) < DateTime.Now.AddDays(-5)) {
-                    logger.Swallow(async () => {
-                        File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "Music", "lavalink.jar"));
-                        await DownloadLavalink();
-                    });
-                }
-
-                logger.Info("Starting self music node");
-                var startInfo = new ProcessStartInfo("java", "-jar lavalink.jar") {
-                    CreateNoWindow = false, RedirectStandardError = false, RedirectStandardInput = false,
-                    RedirectStandardOutput = false, UseShellExecute = true,
-                    WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Music")
-                };
-                var process = Process.Start(startInfo);
-                AppDomain.CurrentDomain.ProcessExit += (sender, args) => process?.Kill();
-                nodes.Add(new LavalinkNodeOptions {
-                    RestUri = $"http://localhost:{GlobalConfig.Instance.SelfPort}/",
-                    WebSocketUri = $"ws://localhost:{GlobalConfig.Instance.SelfPort}/",
-                    Password = GlobalConfig.Instance.SelfPass
-                });
-            }
-
-            nodes.AddRange(GlobalConfig.Instance.LavalinkNodes.Select(instanceLavalinkNode =>
+            var nodes = new List<LavalinkNodeOptions>(GlobalConfig.Instance.LavalinkNodes.Select(instanceLavalinkNode =>
                 new LavalinkNodeOptions {
-                    RestUri = instanceLavalinkNode.RestUri, WebSocketUri = instanceLavalinkNode.WebSocketUri,
+                    RestUri = instanceLavalinkNode.RestUri,
+                    WebSocketUri = instanceLavalinkNode.WebSocketUri,
                     Password = instanceLavalinkNode.Password
                 }));
 
             if (nodes.Count != 0) {
                 logger.Info("Start building music cluster");
-                var lavalinkLogger = new EventLogger();
-                lavalinkLogger.LogMessage += (sender, args) => {
-                    switch (args.Level) {
-                        case LogLevel.Information:
-                            logger.Info(args.Message);
-                            break;
-                        case LogLevel.Error:
-                            logger.Error(args.Exception, args.Message);
-                            break;
-                        case LogLevel.Warning:
-                            logger.Warn(args.Message);
-                            break;
-                        case LogLevel.Debug:
-                            logger.Info(args.Message);
-                            break;
-                        case LogLevel.Trace:
-                            logger.Trace(args.Message);
-                            break;
-                        default:
-                            logger.Warn(args.Message);
-                            break;
-                    }
-                };
                 try {
                     Cluster = new LavalinkCluster(new LavalinkClusterOptions {Nodes = nodes.ToArray()}, new DiscordClientWrapper(Program.Client),
-                        lavalinkLogger) {StayOnline = true};
+                        _lavalinkLogger) {StayOnline = true};
                     Task.Run(async () => {
-                        if (GlobalConfig.Instance.IsSelfMusicEnabled)
-                            await Task.Delay(TimeSpan.FromSeconds(10));
                         logger.Info("Trying to connect to nodes!");
                         await Cluster.InitializeAsync();
 
-                        logger.Info("initializing InactivityTrackingService instance with default options");
+                        logger.Info("Initializing InactivityTrackingService instance with default options");
                         var inactivityTrackingService = new InactivityTrackingService(Cluster, new DiscordClientWrapper(Program.Client),
                             new InactivityTrackingOptions {
                                 TrackInactivity = true,
                                 DisconnectDelay = TimeSpan.FromSeconds(30),
                                 PollInterval = TimeSpan.FromSeconds(4)
-                            }, lavalinkLogger);
+                            }, _lavalinkLogger);
                         inactivityTrackingService.InactivePlayer += async (sender, args) => {
                             if (args.Player is EmbedPlaybackPlayer embedPlaybackPlayer) {
                                 embedPlaybackPlayer.PrepareShutdown(embedPlaybackPlayer.Loc.Get("Music.NoListenersLeft"));
@@ -168,6 +134,7 @@ namespace Bot.Music {
                         lastTrack = null;
                         lavalinkTracks.AddRange(item4.Result);
                     }
+
                     if (item3?.Result == null) continue;
                     if (lastTrack == null || !lastTrack.Title.Contains(s)) {
                         lavalinkTracks.Add(item3.Result);
@@ -202,43 +169,11 @@ namespace Bot.Music {
             builder.Replace("\"", "");
             builder.Replace("#", "");
         }
-
-        public static void WriteResourceToFile(string resourceName, string fileName) {
-            using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)) {
-                using (var file = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
-                    resource.CopyTo(file);
-                }
-            }
-        }
-
-        private static async Task DownloadLavalink() {
-            logger.Info("Downloading latest lavalink.jar");
-
-            try {
-                using (var wc = new WebClient()) {
-                    var previousPercent = 0;
-                    wc.DownloadProgressChanged += (sender, args) => {
-                        logger.Info("Downloading {percentage}%", args.ProgressPercentage);
-                        if (args.ProgressPercentage - previousPercent <= 10) return;
-                        logger.Info("Downloading - {percentage}%", args.ProgressPercentage);
-                        previousPercent = args.ProgressPercentage;
-                    };
-                    wc.DownloadFileCompleted += (sender, args) => { logger.Info(args.Error, "Download completed - {args}"); };
-                    logger.Info("Starting download lavalink.jar");
-                    wc.DownloadFile(new Uri("https://gitlab.com/SKProCH/lavalinkci/-/jobs/artifacts/master-jb/raw/Lavalink.jar?job=build"),
-                        Path.Combine(Directory.GetCurrentDirectory(), "Music", "lavalink.jar"));
-                    logger.Info("End download lavalink.jar");
-                }
-            }
-            catch (Exception e) {
-                logger.Fatal(e, "Can't download lavalink");
-            }
-        }
     }
 
     public class AttachmentAddFailException : Exception { }
 
     public class NothingFoundException : Exception { }
-    
+
     public class EmptyQueryException : Exception { }
 }
