e1a69e93bb1575f44119597a92d8f29666cd9ce5
SKProCH <skproch@yandex.ru>
2020-05-17 20:40:54 +0300

    Add localization command

--- nJMaLBot/Commands/Admin.cs
+++ nJMaLBot/Commands/Admin.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Bot.Config;
 using Bot.Config.Localization;
 using Bot.Utilities;
+using Bot.Utilities.Collector;
 using Bot.Utilities.Commands;
 using Bot.Utilities.Modules;
 using Discord;
@@ -22,9 +23,49 @@ namespace Bot.Commands {
             Context.Message.SafeDelete();
         }
 
+        [Command("language")]
+        [Alias("languages")]
+        [Summary("language0s")]
+        public async Task ListLanguages() {
+            var embedBuilder = this.GetAuthorEmbedBuilder().WithColor(Color.Gold).WithTitle(Loc.Get("Localization.LanguagesList"));
+            foreach (var (key, pack) in Localization.Languages) {
+                embedBuilder.AddField($"{pack.LocalizationFlagEmojiText} **{pack.LocalizedName}** ({pack.LanguageName})",
+                    Loc.Get("Localization.LanguageDescription").Format(GuildConfig.Prefix, key, pack.Authors, pack.TranslationCompleteness), true);
+            }
+
+            var message = await ReplyAsync(null, false, embedBuilder.Build());
+            CollectorsGroup collectors = null;
+            var packsWithEmoji = Localization.Languages.Where(pair => pair.Value.LocalizationFlagEmoji != null).ToList();
+            collectors = new CollectorsGroup(packsWithEmoji.Select(
+                pair => {
+                    var packName = pair.Key;
+                    return CollectorsUtils.CollectReaction(message, reaction => reaction.Emote.Equals(pair.Value.LocalizationFlagEmoji), async args => {
+                        await args.RemoveReason();
+                        var t = await Program.Handler.ExecuteCommand($"setlanguage {packName}", new ReactionCommandContext(Program.Client, args.Reaction),
+                            args.Reaction.UserId.ToString());
+                        if (t.IsSuccess) {
+                            message.SafeDelete();
+                            collectors?.DisposeAll();
+                        }
+                    }, CollectorFilter.IgnoreBots);
+                }));
+            try {
+                await message.AddReactionsAsync(packsWithEmoji.Select(pair => pair.Value.LocalizationFlagEmoji).ToArray());
+            }
+            catch (Exception e) {
+                // ignored
+            }
+            message.DelayedDelete(TimeSpan.FromMinutes(5));
+        }
+
         [Command("setlanguage")]
         [Summary("setlanguage0s")]
-        public async Task SetLanguage([Summary("setlanguage0_0s")] string language) {
+        public async Task SetLanguage([Summary("setlanguage0_0s")] string language = null) {
+            if (language == null) {
+                await ListLanguages();
+                return;
+            }
+
             if (Localization.Languages.ContainsKey(language)) {
                 GuildConfig.Get(Context.Guild.Id).SetLanguage(language).Save();
                 Context.Message.SafeDelete();
--- nJMaLBot/Config/Localization/LocalizationPack.cs
+++ nJMaLBot/Config/Localization/LocalizationPack.cs
@@ -1,11 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Discord;
 using Newtonsoft.Json;
 
 namespace Bot.Config.Localization {
     public class LocalizationPack {
+        private string _localizationFlagEmojiText;
         public string LanguageName { get; set; }
         public string LocalizedName { get; set; }
-        public string LocalizationFlagEmoji { get; set; }
+
+        [JsonProperty("LocalizationFlagEmoji")]
+        public string LocalizationFlagEmojiText {
+            get => _localizationFlagEmojiText;
+            set {
+                _localizationFlagEmojiText = value;
+                try {
+                     LocalizationFlagEmoji = new Emoji(value);
+                }
+                catch (Exception e) {
+                    // ignored
+                }
+            }
+        }
+
+        [JsonIgnore]
+        public IEmote LocalizationFlagEmoji { get; set; }
         public string FallbackLanguage { get; set; }
         public string Authors { get; set; }
         public Dictionary<string, Dictionary<string, string>> Data { get; set; }
