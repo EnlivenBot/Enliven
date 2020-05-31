ba9f632ed3b8ac10449a39ea725064bec932b16d
SKProCH <skproch@yandex.ru>
2020-05-31 18:34:41 +0300

    Add format support to LocalizedEntry

--- nJMaLBot/Config/Localization/LocalizedEntry.cs
+++ nJMaLBot/Config/Localization/LocalizedEntry.cs
@@ -1,4 +1,8 @@
-﻿using Bot.Config.Localization.Providers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bot.Config.Localization.Providers;
+using Bot.Utilities;
 
 namespace Bot.Config.Localization {
     public class LocalizedEntry {
@@ -8,12 +12,34 @@ namespace Bot.Config.Localization {
         }
 
         public LocalizedEntry(string id) : this(id.Split(".")[0], id.Split(".")[1]) { }
+
+        private bool _isCalculated = false;
+        private List<Func<object>> FormatArgs { get; set; } = new List<Func<object>>();
         
         public string Group { get; set; }
         public string Id { get; set; }
 
+        private ILocalizationProvider _provider;
+        private string _cache;
         public string Get(ILocalizationProvider provider) {
-            return provider.Get(Group, Id);
+            if (_isCalculated || _provider != provider) {
+                _cache = provider.Get(Group, Id).Format(FormatArgs.Select(func => func()));
+                _provider = provider;
+            }
+            
+            return _cache;
+        }
+
+        public LocalizedEntry Add(params string[] args) {
+            FormatArgs.AddRange(args.ToList().Select<string, Func<string>>(s => () => s));
+
+            return this;
+        }
+        
+        public LocalizedEntry Add(params Func<string>[] args) {
+            _isCalculated = true;
+            FormatArgs.AddRange(args);
+            return this;
         }
     }
 }
