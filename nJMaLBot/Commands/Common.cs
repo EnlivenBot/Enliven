359e7d04d083f07a510d56837afeded839d851e2
SKProCH <skproch@yandex.ru>
2020-06-04 09:03:05 +0300

    Add vote info to headers

--- nJMaLBot/Commands/Common.cs
+++ nJMaLBot/Commands/Common.cs
@@ -58,6 +58,7 @@ namespace Bot.Commands {
 
     public class CommonCommands : AdvancedModuleBase {
         [Command("vote")]
+        [Alias("support", "voting")]
         public async Task Vote() {
             Context.Message.SafeDelete();
             await ReplyFormattedAsync(Loc.Get("Common.Vote"), Loc.Get("Common.VoteDescription"));
