69908b5f13b22658e2842295e17162acb7c370e7
SKProCH <skproch@yandex.ru>
2020-07-15 09:46:23 +0300

    Add IsExecuting property to SingleTask

--- nJMaLBot/Utilities/SingleTask.cs
+++ nJMaLBot/Utilities/SingleTask.cs
@@ -13,6 +13,8 @@ namespace Bot.Utilities {
         public readonly Func<Task<T>> Action;
         private readonly object _lockObject = new object();
         private TaskCompletionSource<T>? _taskCompletionSource;
+        
+        public bool IsExecuting { get; private set; }
 
         public SingleTask(Func<T> action) {
             Action = () => Task.FromResult(action());
@@ -27,8 +29,10 @@ namespace Bot.Utilities {
                 if (_taskCompletionSource != null) return _taskCompletionSource.Task;
                 _taskCompletionSource = new TaskCompletionSource<T>();
                 _ = Task.Run(async () => {
+                    IsExecuting = true;
                     var result = await Action();
                     lock (_lockObject) {
+                        IsExecuting = false;
                         var localTaskCompletionSource = _taskCompletionSource;
                         _taskCompletionSource = null;
                         localTaskCompletionSource.SetResult(result);
