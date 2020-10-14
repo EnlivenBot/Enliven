c849d644e3a83c900f4fe51dd3ed8cb901e68ced
SKProCH <skproch@yandex.ru>
2020-10-14 09:59:47 +0300

    Remove ObsoleteStatisticsPart

--- nJMaLBot/Config/StatisticsPart.cs
+++ nJMaLBot/Config/StatisticsPart.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using LiteDB;
 
 namespace Bot.Config {
     public class StatisticsPart {
         [BsonId] public string Id { get; set; } = null!;
         public Dictionary<string, int> UsagesList { get; set; } = new Dictionary<string, int>();
-    }
-    
-    [Obsolete("Class deprecated, left for backward compability. Use StatisticsPart")]
-    public class ObsoleteStatisticsPart {
-        [BsonId] public string Id { get; set; } = null!;
-        public Dictionary<string, long> UsagesList { get; set; } = new Dictionary<string, long>();
     }
 }
