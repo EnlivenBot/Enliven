8a55374703f454f51cf690671e2c9bbf816f8ae1
SKProCH <skproch@yandex.ru>
2021-08-29 22:04:52 +0300

    Add resolving user effect in EffectSourceProvider

--- Common/Music/Effects/EffectSourceProvider.cs
+++ Common/Music/Effects/EffectSourceProvider.cs
@@ -1,15 +1,25 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Common.Config;
+using LiteDB;
 
 namespace Common.Music.Effects {
     public class EffectSourceProvider {
+        private readonly IUserDataProvider _userDataProvider;
+        public EffectSourceProvider(IUserDataProvider userDataProvider) {
+            _userDataProvider = userDataProvider;
+        }
+        
         public bool TryResolve(UserLink? link, string effectName, [NotNullWhen(true)] out IPlayerEffectSource? source) {
             if (PlayerEffectSource.DefaultEffectsMap.TryGetValue(effectName, out source)) {
                 return true;
             }
-            
-            // TODO: Implement custom user effects resolving
-            return false;
+
+            var userData = link?.GetData(_userDataProvider);
+            var userEffect = userData?.PlayerEffects.LastOrDefault(effect => effect.DisplayName == effectName || effect.SourceName == effectName);
+            if (userEffect == null) return false;
+            source = new PlayerEffectSource(userEffect);
+            return true;
         }
         
         public IPlayerEffectSource? Resolve(UserLink? link, string effectName) {
