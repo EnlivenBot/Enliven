142edca5c1b5cb8896f48b6bf7a302228de8a14a
SKProCH <skproch@yandex.ru>
2022-01-04 18:42:21 +0300

    Change invite command in favor of "Add to server" discord button and GitLab link

--- Enliven/Commands/UtilsCommand.cs
+++ Enliven/Commands/UtilsCommand.cs
@@ -10,10 +10,9 @@ namespace Bot.Commands {
         [Command("invite", RunMode = RunMode.Async)]
         [Alias("link")]
         [Summary("invite0s")]
-        public async Task Invite([Summary("invite0_0s")] bool emptyPermissions = false) {
+        public async Task Invite() {
             Context.Message.SafeDelete();
-            var inviteUrl = $"https://discordapp.com/api/oauth2/authorize?client_id={Context.Client.CurrentUser.Id}&permissions=1110764608&scope=bot";
-            await ReplyFormattedAsync(Loc.Get("Common.Invite"), Loc.Get("Common.InviteDescription").Format(inviteUrl));
+            await ReplyFormattedAsync(Loc.Get("Common.Invite"), Loc.Get("Common.InviteDescription"));
         }
     }
 }
