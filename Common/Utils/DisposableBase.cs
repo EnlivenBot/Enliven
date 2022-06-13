b7dbe162dfbdabe39b4f5b69c09fe033106b2e0d
SKProCH <skproch@yandex.ru>
2022-06-13 23:59:37 +0300

    Extend DisposableBase

--- Common/Utils/DisposableBase.cs
+++ Common/Utils/DisposableBase.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 
 namespace Common.Utils {
     public interface IDisposableBase : IDisposable {
+        Task DisposedTask { get; }
         IObservable<IDisposableBase> Disposed { get; }
         bool IsDisposed { get; }
     }
 
     public abstract class DisposableBase : IDisposableBase {
         private readonly Subject<IDisposableBase> _disposed = new();
+        private Task? _disposedTask;
+        public Task DisposedTask => _disposedTask ??= Disposed.ToTask();
         public IObservable<IDisposableBase> Disposed => _disposed.AsObservable();
         public bool IsDisposed { get; private set; }
         public virtual void Dispose() {
@@ -40,6 +44,8 @@ namespace Common.Utils {
 
     public abstract class AsyncDisposableBase : IDisposableBase, IAsyncDisposable {
         private readonly Subject<IDisposableBase> _disposed = new();
+        private Task? _disposedTask;
+        public Task DisposedTask => _disposedTask ??= Disposed.ToTask();
         public IObservable<IDisposableBase> Disposed => _disposed.AsObservable();
         public bool IsDisposed { get; private set; }
         public virtual void Dispose() {
