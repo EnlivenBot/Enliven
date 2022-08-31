772d9ed4bcc5ffc0cdec74e870aaf33ee9fd69c6
SKProCH <skproch@yandex.ru>
2022-09-01 02:15:36 +0300

    Add additional extension methods to IEntry, add static Success and Fail properties to EntryLocalized

--- Common/Localization/Entries/EntryLocalized.cs
+++ Common/Localization/Entries/EntryLocalized.cs
@@ -3,6 +3,8 @@ using Common.Localization.Providers;
 
 namespace Common.Localization.Entries {
     public class EntryLocalized : EntryString {
+        public static readonly IEntry Success = new EntryLocalized("Commands.Success");
+        public static readonly IEntry Fail = new EntryLocalized("Commands.Fail");
         public EntryLocalized(string id) : base(id) { }
 
         public EntryLocalized(string id, params object[] args) : base(id, args) { }
