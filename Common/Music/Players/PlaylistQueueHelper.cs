def8db6db341c8af6952846c0db4316128394c09
SKProCH <skproch@yandex.ru>
2023-05-28 18:51:40 +0300

    Fix Index was out of range for queue display

--- Common/Music/Players/PlaylistQueueHelper.cs
+++ Common/Music/Players/PlaylistQueueHelper.cs
@@ -6,18 +6,22 @@ using Lavalink4NET.Player;
 
 namespace Common.Music.Players {
     public sealed class PlaylistQueueHelper : IEnumerator<LavalinkTrack> {
-        private readonly LavalinkPlaylist _playlist;
         private readonly int _endIndex;
-        private int _currentIndex;
-        private LavalinkTrack? _previous;
+        private readonly LavalinkPlaylist _playlist;
         private LavalinkTrack _current = null!;
+        private int _currentIndex;
         private LavalinkTrack? _next;
+        private LavalinkTrack? _previous;
         public PlaylistQueueHelper(LavalinkPlaylist playlist, int beginIndex, int count) {
             _playlist = playlist;
             _endIndex = beginIndex + count;
             _currentIndex = Math.Max(beginIndex, 0) - 1;
-            _next = playlist[_currentIndex + 1];
+            playlist.TryGetValue(_currentIndex + 1, out _next);
         }
+        public int CurrentTrackNumber => _currentIndex + 1;
+        public int CurrentTrackIndex => _currentIndex;
+        public bool IsFirstInGroup => _previous == null || _previous.GetRequester() != _current.GetRequester();
+        public bool IsLastInGroup => _next == null || _next.GetRequester() != _current.GetRequester();
         public bool MoveNext() {
             if (_currentIndex >= _endIndex)
                 return false;
@@ -35,10 +39,6 @@ namespace Common.Music.Players {
             throw new NotSupportedException();
         }
         public LavalinkTrack Current => _current;
-        public int CurrentTrackNumber => _currentIndex + 1;
-        public int CurrentTrackIndex => _currentIndex;
-        public bool IsFirstInGroup => _previous == null || _previous.GetRequester() != _current.GetRequester();
-        public bool IsLastInGroup => _next == null || _next.GetRequester() != _current.GetRequester();
 
         object IEnumerator.Current => Current;
 
