e0f36af9b5bb0183e433fd16ed68ed2bdfcdb614
SKProCH <skproch@yandex.ru>
2025-05-07 20:04:04 +0300

    Add traces instrumentation for InteractionHandlerService

--- Enliven/DiscordRelated/Interactions/InteractionHandlerService.cs
+++ Enliven/DiscordRelated/Interactions/InteractionHandlerService.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using Bot.Infrastructure.Tracing;
 using Common;
 using Discord;
 using Discord.Interactions;
 using Microsoft.Extensions.Logging;
+using SerilogTracing.Instrumentation;
 
 namespace Bot.DiscordRelated.Interactions;
 
 public class InteractionHandlerService : IService, IDisposable
 {
+    private static readonly ActivitySource ActivitySource = new("Enliven.InteractionHandlerService");
+    
     private readonly CustomInteractionService _customInteractionService;
     private readonly EnlivenShardedClient _enlivenShardedClient;
     private readonly ILogger<InteractionHandlerService> _logger;
@@ -43,24 +48,40 @@ public class InteractionHandlerService : IService, IDisposable
 
     private async Task OnInteractionCreated(ShardedInteractionContext context)
     {
+        Activity.Current = null;
+        using var activity = ActivitySource.StartActivityStructured(ActivityKind.Server, 
+            "Received discord interaction from {User} ({UserId}) in {GuildId}-{ChannelId}", 
+            context.User.Username, context.User.Id, context.Guild?.Id, context.Channel.Id);
+        activity?.AddTag("InteractionType", GetInteractionType(context));
         try
         {
             var interactionSearchResult = SearchInteraction(context);
             if (!interactionSearchResult.IsSuccess)
             {
-                _logger.LogWarning("Interaction not found. Id: {InteractionId}. Reason: {Reason}",
+                _logger.LogWarning("Interaction {InteractionText} not found due to {Reason}",
                     interactionSearchResult.Text, interactionSearchResult.ErrorReason);
+                activity?.SetStatus(ActivityStatusCode.Error);
                 return;
             }
+            
+            _logger.LogDebug("Interaction successfully resolved to {Command}", interactionSearchResult.Command.Name);
 
-            var result = await interactionSearchResult.Command.ExecuteAsync(context, _serviceProvider)
+            var result = await interactionSearchResult.Command
+                .ExecuteAsync(context, _serviceProvider)
                 .ConfigureAwait(false);
             if (!result.IsSuccess)
             {
                 var exception = result is ExecuteResult executeResult ? executeResult.Exception : null;
                 if (exception is not CommandInterruptionException)
-                    _logger.LogError(exception, "Interaction execution {Result}: {Reason}", result.Error!.Value,
-                        result.ErrorReason);
+                {
+                    _logger.LogError(exception, "Interaction {Command} failed ({InteractionResult}) due to {Reason}", 
+                        interactionSearchResult.Command.Name, result.Error!.Value, result.ErrorReason);
+                    activity?.SetStatus(ActivityStatusCode.Error);
+                }
+                if (exception is not CommandInterruptionException && exception is not null)
+                {
+                    activity?.AddException(exception);
+                }
             }
 
             try
@@ -77,7 +98,25 @@ public class InteractionHandlerService : IService, IDisposable
         catch (Exception e)
         {
             _logger.LogError(e, "Error while handling interaction");
+            if (activity is not null)
+            {
+                ActivityInstrumentation.TrySetException(activity, e);
+                activity.SetStatus(ActivityStatusCode.Error);
+            }
         }
+    }
+    
+    private string GetInteractionType(ShardedInteractionContext context)
+    {
+        return context.Interaction switch
+        {
+            ISlashCommandInteraction => "ISlashCommandInteraction",
+            IComponentInteraction => "IComponentInteraction",
+            IUserCommandInteraction => "IUserCommandInteraction",
+            IMessageCommandInteraction => "IMessageCommandInteraction",
+            IAutocompleteInteraction => "IAutocompleteInteraction",
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 
     private SearchResult<ICommandInfo> SearchInteraction(ShardedInteractionContext context)
---
+++ Enliven/Infrastructure/Tracing/ActivitySourceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JetBrains.Annotations;
+using Serilog.Parsing;
+
+#pragma warning disable CA2254
+
+namespace Bot.Infrastructure.Tracing;
+
+public static class ActivitySourceExtensions
+{
+    private static readonly MessageTemplateParser MessageTemplateParser = new();
+    private static readonly Dictionary<string, string[]> ParsedCache = new();
+
+    public static Activity? StartActivityStructured(this ActivitySource source, ActivityKind activityKind,
+        [StructuredMessageTemplate] string message, params Span<object?> args)
+    {
+        return StartActivityStructured(source, activityKind, default, message, args);
+    }
+
+    public static Activity? StartActivityStructured(this ActivitySource source, ActivityKind activityKind,
+        ActivityContext activityContext, [StructuredMessageTemplate] string message, params Span<object?> args)
+    {
+        if (!ParsedCache.TryGetValue(message, out var parsedArgs))
+        {
+            var template = MessageTemplateParser.Parse(message);
+            parsedArgs = template.Tokens
+                .OfType<PropertyToken>()
+                .Select(token => token.PropertyName)
+                .ToArray();
+
+            ParsedCache[message] = parsedArgs;
+        }
+
+        if (parsedArgs.Length != args.Length)
+        {
+            throw new Exception("Invalid number of arguments");
+        }
+
+        var activity = source.StartActivity(message, activityKind, activityContext);
+        if (activity is null || args.Length <= 0) return activity;
+
+        for (var i = 0; i < parsedArgs.Length; i++)
+        {
+            var argName = parsedArgs[i];
+            activity?.SetTag(argName, args[i]);
+        }
+
+        return activity;
+    }
+}
